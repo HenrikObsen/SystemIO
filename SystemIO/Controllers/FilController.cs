d4af23e6c034917d8f6352946b5e169957bd637c
HenrikObsen <hto@djes.dk>
2015-11-16 12:27:06 +0100

    New

---
+++ SystemIO/Controllers/FilController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using SystemIO.Helpers;
+
+namespace SystemIO.Controllers
+{
+    public class FilController : Controller
+    {
+        FileTools ft = new FileTools();
+
+        public ActionResult CreateFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateFile(string fileName)
+        {
+            string filePath = Request.PhysicalApplicationPath + "/Filer/" + fileName;
+            ViewBag.MSG = ft.CreateFile(filePath);
+
+            return View("CreateFile");
+        }
+
+
+    }
+}
---
+++ SystemIO/Helpers/FileTools.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace SystemIO.Helpers
+{
+    public class FileTools
+    {
+        public string CreateFile(string filePath)
+        {
+            StreamWriter sw = File.CreateText(filePath);
+            sw.Close();
+            return "Filen er oprettet";
+        }
+
+    }
+}
+
