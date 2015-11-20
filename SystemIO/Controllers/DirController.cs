f8e90339b3eec8ffaebe86d5ebe40236669f4815
hto@djes.dk <Obsengud78>
2015-11-20 10:22:43 +0100

    test

---
+++ SystemIO/Controllers/DirController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SystemIO.Controllers
+{
+    public class DirController : Controller
+    {
+        // GET: Dir
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
